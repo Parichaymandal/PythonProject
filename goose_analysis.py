f12b02d94a2903f5f673f82c6c5753aad5305dc8
Juan Sebastian Garzón Alvarado <js.garzon398@uniandes.edu.co>
2020-07-02 11:11:40 +0200

    Array with timestamp and timeseries plot (#10)
    * Array with timestamp and timeseries plot
    
    * Remove printig of times and folder typo

--- goose_analysis.py
+++ goose_analysis.py
@@ -1,6 +1,7 @@
 import numpy as np
 from qgis.utils import iface
 from qgis.core import *
+from datetime import datetime
 import os
 from analysis.anova import *
 from preprocessing.stack_tiff import *
@@ -8,13 +9,15 @@ from preprocessing.import_shapefile import *
 from preprocessing.get_temperature import *
 from preprocessing.import_raster import *
 from preprocessing.get_arrays import *
+from postprocessing.time_series import *
 from postprocessing.heatmap import *
+
 
 ##################### IMPORTANT ##########################
 
 #The code only works if the project folder path is defined#
 #Modify the example below #
-project_folder=os.path.join('/Users','PythonProject')
+project_folder=os.path.join('/Users','sebastiangarzon','Desktop','PythonProject')
                           
 ####################### PREPROCESSING #########################
 
@@ -22,6 +25,7 @@ project_folder=os.path.join('/Users','PythonProject')
 raster_folder = os.path.join(project_folder,'databases','raster')
 shp_folder=os.path.join(project_folder,'databases','shapefiles')
 raster_tif_path=os.path.join(raster_folder,'stacked.tif')
+figures_folder=os.path.join(project_folder,'figures')
 
 #### Stack rasters ####
 single_tif_to_stacked(raster_folder)
@@ -36,7 +40,8 @@ temp_raster=import_raster(raster_tif_path)
 get_month_band_temperature(shp_points,temp_raster)
 
 #### Extract arrays for analysis ####
-x, y, ids, months, years, temps = extract_arrays(shp_points, excludeNone = True)
+x, y, ids, months, years, temps,timestamps = extract_arrays(shp_points, excludeNone = True)
+
 
 ####################### ANALYSIS #########################
 
@@ -61,6 +66,12 @@ F, pval = repeated_measures_oneway_anova(temp, month, ind)
 
 ####################### POST-PROCESSING #########################
 
+
+# Timeseries plot
+timeseries_path=os.path.join(figures_folder,'timeseries_geese.png')
+plot_timeseries(ids,timestamps,temps,timeseries_path)
+
+
 # Dynamic heatmap
-figpath = os.path.join(project_folder,'figures','monthly_heatmap.png')
+figpath = os.path.join(figures_folder,'monthly_heatmap.png')
 monthly_heatmap(x, y, months, 5, 25, figpath)
