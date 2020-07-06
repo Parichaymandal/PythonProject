ae55bb25e40974902a32f27c31a630ff0c37813b
Juan Sebastian Garzón Alvarado <js.garzon398@uniandes.edu.co>
2020-07-06 17:58:33 +0200

    fix path

--- goose_analysis.py
+++ goose_analysis.py
@@ -18,7 +18,7 @@ from postprocessing.individuals_distribution import *
 
 #The code only works if the project folder path is defined#
 #Modify the example below #
-project_folder=os.path.join('/Users','sebastiangarzon','Desktop','PythonProject')
+project_folder=os.path.join('/Users','PythonProject')
                           
 ####################### PREPROCESSING #########################
 
